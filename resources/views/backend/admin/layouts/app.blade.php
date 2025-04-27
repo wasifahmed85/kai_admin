cbb812697a362462a76ce5efb89ab00526d2872b
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-04-27 17:38:12 +0600

    useless link  remove

--- resources/views/backend/admin/layouts/app.blade.php
+++ resources/views/backend/admin/layouts/app.blade.php
@@ -37,8 +37,6 @@
     <link rel="stylesheet" href="{{ asset('assets/css/plugins.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/kaiadmin.min.css') }}" />
 
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
 </head>
 
 <body>
